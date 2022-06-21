7d003e24af3904a493f04c3bd9a788748bf27451
Kapil Sharma <ksharma120497@gmail.com>
2022-06-22 01:28:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import React from 'react';
 import './App.css';
+import MainContainer from "./components/MainContainer";
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
+import { Login } from './components/user-auth/Login';
+import {SignUp} from './components/user-auth/SignUp';
 
 function App() {
   return (
     <div>
-     Hello World!
+    <Routes>
+      <Route path="/" element={<MainContainer/>}></Route>
+      <Route path="/login" element={<Login/>}></Route>
+      <Route path="/signup" element={<SignUp/>}></Route>
+    </Routes>
     </div>
   );
 }
